c003eadaa93f7b27bd732eed2836f2eb4b0b24f8
sammiex0x <sammiex0x.sammiex0x@hotmail.com>
2017-02-21 22:58:50 -0800

    hw6

---
+++ pizza.js
@@ -0,0 +1,70 @@
+var start = 3;
+
+function mush() {
+
+console.log("I'm a mushroom lover");
+
+var currTotal = document.getElementById("tot").innerHTML;
+    if (isNaN(currTotal)) {
+        currTotal = start;
+    }
+
+
+    var newTotal = 2 + +currTotal;
+
+    document.getElementById("tot").innerHTML = newTotal;
+}
+var start = 3;
+
+function bac() {
+
+console.log("I'm a Bacon lover");
+
+var currTotal = document.getElementById("tot").innerHTML;
+    if (isNaN(currTotal)) {
+        currTotal = start;
+    }
+
+
+    var newTotal = 3 + +currTotal;
+
+    document.getElementById("tot").innerHTML = newTotal;
+}
+
+var start = 3;
+
+function pep() {
+
+console.log("I'm a pep lover");
+
+var currTotal = document.getElementById("tot").innerHTML;
+    if (isNaN(currTotal)) {
+        currTotal = start;
+    }
+
+
+    var newTotal = 1 + +currTotal;
+
+    document.getElementById("tot").innerHTML = newTotal;
+}
+
+var start = 3;
+
+function ham() {
+
+console.log("I'm a ham lover");
+
+var currTotal = document.getElementById("tot").innerHTML;
+    if (isNaN(currTotal)) {
+        currTotal = start;
+    }
+
+    var newTotal = 2 + +currTotal;
+
+    document.getElementById("tot").innerHTML = newTotal;
+}
+
+function CL() {
+    console.log("CL");
+     document.getElementById("tot").innerHTML = "0";
+}
